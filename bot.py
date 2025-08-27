73ee3a2b8601a83b7577fa82428c48cb7a054141
riohkk <khoirrizqih@gmail.com>
2025-08-27 18:53:31 +0700

    komentar komitmen

--- bot.py
+++ bot.py
@@ -35,4 +35,10 @@ async def ban_error(ctx, error):
     elif isinstance(error, commands.MemberNotFound):
         await ctx.send("User not found.")
 
+@bot.event
+async def on_member_join(member):
+    # Mengirim pesan ucapan selamat
+    for channel in member.guild.text_channels:
+        await channel.send(f'Selamat datang, {member.mention}!')
+
 bot.run(token)
